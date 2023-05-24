a02fdfb6a603cd47e70cee6f9c80d648e8c88570
Ronald Barasa <barasaogoro@gmail.com>
2023-05-24 13:57:00 +0300

    completed assignt

--- lib/person.rb
+++ lib/person.rb
@@ -9,3 +9,15 @@ class Person
     end
   end
 end
+bob_attributes = { name: "Bob", hair_color: "Brown" }
+
+bob = Person.new(bob_attributes)
+puts bob.name       # => "Bob"
+puts bob.hair_color # => "Brown"
+
+susan_attributes = { name: "Susan", height: "5'11\"", eye_color: "Green" }
+
+susan = Person.new(susan_attributes)
+puts susan.name      # => "Susan"
+puts susan.height    # => "5'11""
+puts susan.eye_color # => "Green"
